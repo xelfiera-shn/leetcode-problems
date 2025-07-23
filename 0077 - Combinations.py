36bf45ad9258f4bbca05b975a01bda3982642465
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-07-23 17:00:54 +0300

    First try for problem 77

--- 0077 - Combinations.py
+++ 0077 - Combinations.py
@@ -1,6 +1,18 @@
 class Solution(object):
     @staticmethod
     def combine(n, k):
-        candidate = []
-        for i in range(n):
-            pass
+        combinations = []
+        GenerateCombinations(combinations, [], 0, n, k)
+
+        return combinations
+
+def GenerateCombinations(combinations, candidate, index, n, k):
+    if len(candidate) == k:
+        combinations.append(candidate)
+        print(candidate)
+
+    for i in range(index, n):
+        candidate += [i + 1]
+        GenerateCombinations(combinations, candidate, i + 1, n, k)
+
+Solution.combine(4, 2)
