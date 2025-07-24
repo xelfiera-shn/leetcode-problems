791a0834e6d786ed48a4664bba6a977bf57f6f99
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-07-24 22:54:36 +0300

    Finished problem 77 solving

--- 0077 - Combinations.py
+++ 0077 - Combinations.py
@@ -10,9 +10,36 @@ def GenerateCombinations(combinations, candidate, index, n, k):
     if len(candidate) == k:
         combinations.append(candidate)
         return
-
+    
     for i in range(index, n):
-        candidate += [i + 1]
-        GenerateCombinations(combinations, candidate.copy(), i + 1, n, k)
+        newCandidate = candidate.copy()
+        newCandidate.append(i + 1)
+        GenerateCombinations(combinations, newCandidate, i + 1, n, k)
 
-print(Solution.combine(4, 2))
+# Another backtrack solution
+class Solution:
+    @staticmethod
+    def combine(n, k):
+        results = []
+        candidate = []
+
+        def backtrack(start):
+            if len(candidate) == k:
+                results.append(candidate[ : ])
+                return
+            
+            for num in range(start, n + 1):
+                candidate.append(num)
+                backtrack(num + 1)
+                candidate.pop()
+
+        backtrack(1)
+        return results
+
+# Another solution
+from itertools import combinations
+
+class Solution:
+    @staticmethod
+    def combine(n, k):
+        return (list(combinations(range(1,n+1), k)))
