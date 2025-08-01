f81f0637c6f1c71100b743949f111529688d4233
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-08-01 23:37:46 +0300

    Problem 55 first try

--- 0055 - JumpGame.py
+++ 0055 - JumpGame.py
@@ -1,4 +1,14 @@
+# Time limit exceeded
 class Solution(object):
     @staticmethod
     def canJump(nums):
-        pass
+        def jump(index):
+            if index == len(nums) - 1: return True
+            if nums[index] == 0: return False
+
+            for i in range(1, nums[index] + 1):
+                if jump(index + i): return True
+                
+            return False
+        
+        return jump(0)
