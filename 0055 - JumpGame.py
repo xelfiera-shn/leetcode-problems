50604281627c2ce973d05a5e3890eccf6cf3e38c
AhmetSahin <ahmet.sahin9663@gmail.com>
2025-08-02 23:54:27 +0300

    Problem 55 dp memoizatıon solution but stıll time limit exceeded

--- 0055 - JumpGame.py
+++ 0055 - JumpGame.py
@@ -1,7 +1,25 @@
-# Time limit exceeded
+# Dynamic programming memoization (Time limit exceeded)
 class Solution(object):
     @staticmethod
     def canJump(nums):
+        memory = [0 for _ in range(len(nums))]
+        memory[-1] = 1 # Set true last index
+
+        def setJumpablity(index):
+            if memory[index] != 0: return 1
+
+            for i in range(1, nums[index] + 1):
+                if setJumpablity(index + i):
+                    memory[index] = 1
+                    return 1
+
+        setJumpablity(0)
+        return True if memory[0] == 1 else False
+
+# Backtrack - Recursive (Time limit exceeded)
+class Solution1(object):
+    @staticmethod
+    def canJump2(nums):
         def jump(index):
             if index == len(nums) - 1: return True
             if nums[index] == 0: return False
