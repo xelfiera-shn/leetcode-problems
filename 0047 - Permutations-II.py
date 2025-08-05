15684a36b2943254f4fbefdffc05e34cfd223eb3
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-05 17:24:48 +0300

    Problem 47 solved with another method

--- 0047 - Permutations-II.py
+++ 0047 - Permutations-II.py
@@ -1,3 +1,36 @@
+class Solution(object):
+    @staticmethod
+    def permuteUnique(nums):
+        permutations = []
+
+        def generatePermutations(numbers, candidate):
+            if len(candidate) == len(nums):
+                permutations.append(candidate)
+                return
+            
+            lastCheckpoint = None
+            for i in range(len(numbers)):
+                if numbers[i] == lastCheckpoint:
+                    continue
+
+                else:
+                    newCandidate = candidate[ : ]
+                    remainNumbers = numbers[ : ]
+
+                    newCandidate.append(remainNumbers[i])
+                    remainNumbers.pop(i)
+
+                    generatePermutations(remainNumbers, newCandidate)
+
+                    lastCheckpoint = numbers[i]
+            
+        nums.sort()
+        generatePermutations(nums, [])
+
+        return permutations
+    
+print(Solution.permuteUnique([0, 4, 1, 1]))
+
 # The solution where performance doesn't matter
 class Solution(object):
     @staticmethod
