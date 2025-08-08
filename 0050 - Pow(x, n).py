fe36085dde75b7c37aaa1eb48de38e666fb09050
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-08 20:45:56 +0300

    Problem 50 function static setted

--- 0050 - Pow(x, n).py
+++ 0050 - Pow(x, n).py
@@ -1,9 +1,10 @@
 class Solution:
-    def myPow(self, x, n):
+    @staticmethod
+    def myPow(x, n):
         if not n: return 1
-        if n < 0: return 1 / self.myPow(x, -n)
-        if n % 2: return x * self.myPow(x, n - 1)
-        else: return self.myPow(x * x, n / 2)
+        if n < 0: return 1 / Solution.myPow(x, -n)
+        if n % 2: return x * Solution.myPow(x, n - 1)
+        else: return Solution.myPow(x * x, n / 2)
 
 # Another solution with Python
 class Solution(object):
