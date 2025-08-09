b1692e0f3d7f1ca9e4421ddc263dd7f23c4c7382
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-09 23:26:10 +0300

    Added whitespaces on problem 48

--- 0048 - RotateImage.py
+++ 0048 - RotateImage.py
@@ -34,10 +34,10 @@ class Solution(object):
         # Transpose matrix
         n = len(matrix)
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
         # Reverse matrix
         for i in range(n):
-            for j in range(n//2):
-                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
