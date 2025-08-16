eb1dd381cf5e5dea96d251b51fb9a2a16fe67b99
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-16 23:12:42 +0300

    Problem 53 solving started

---
+++ 0053 - MaximumSubarray.py
@@ -0,0 +1,19 @@
+# Time limit exceeded solution
+class Solution(object):
+    @staticmethod
+    def maxSubArray(nums):
+        maxValue = nums[0]
+
+        for xIndex in range(len(nums)):
+            localSum = 0
+
+            yIndex = xIndex
+            while yIndex < len(nums):
+                localSum += nums[yIndex]
+
+                if localSum > maxValue:
+                    maxValue = localSum
+
+                yIndex += 1
+
+        return maxValue
