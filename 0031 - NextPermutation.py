731c3dd3fc054f2973b42eed0ae0cc0a5f69a98c
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-18 23:27:38 +0300

    First try of problem 31

--- 0031 - NextPermutation.py
+++ 0031 - NextPermutation.py
@@ -1,8 +1,4 @@
 class Solution(object):
     @staticmethod
     def nextPermutation(nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        
+        pass
