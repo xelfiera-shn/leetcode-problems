af142ea46d3fc661d3e85241855316de88767e62
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-18 23:27:15 +0300

    Problem 31 method static casted

---
+++ 0031 - NextPermutation.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    @staticmethod
+    def nextPermutation(nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
