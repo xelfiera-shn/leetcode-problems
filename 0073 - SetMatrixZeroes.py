51f2d0739c6784a6407e9bcc9e26354eaf516e44
xelfiera <ahmet.sahin9663@gmail.com>
2025-08-24 22:45:44 +0300

    Solved writing issues at problem 73

--- 0073 - SetMatrixZeroes.py
+++ 0073 - SetMatrixZeroes.py
@@ -2,18 +2,18 @@ class Solution(object):
     @staticmethod
     def setZeroes(matrix):
         markedRows = []
-        markedColums = []
+        markedColumns = []
 
         for y in range(len(matrix)):
             for x in range(len(matrix[0])):
                 if matrix[y][x] == 0:
                     if y not in markedRows: markedRows.append(y)
-                    if x not in markedColums: markedColums.append(x)
+                    if x not in markedColumns: markedColumns.append(x)
 
         for row in markedRows:
             for column in range(len(matrix[0])):
                 matrix[row][column] = 0
 
-        for column in markedColums:
+        for column in markedColumns:
             for row in range(len(matrix)):
                 matrix[row][column] = 0
