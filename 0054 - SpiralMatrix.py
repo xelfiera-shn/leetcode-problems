f430fbadfd10db23ecee2baebb5274fc63075231
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-02 17:19:24 +0300

    Problem 54 solved

---
+++ 0054 - SpiralMatrix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    @staticmethod
+    def spiralOrder(matrix):
+        matrixHeight = len(matrix)
+        matrixWidth = len(matrix[0])
+        dpMatrix = [[False] * matrixWidth for _ in range(matrixHeight)]
+
+        orderList = []
+        x, y, dx, dy = 0, 0, 1, 0
+        for _ in range(matrixHeight * matrixWidth):
+            orderList.append(matrix[y][x])
+            dpMatrix[y][x] = True
+
+            if dpMatrix[(y + dy) % matrixHeight][(x + dx) % matrixWidth]:
+                dy, dx = dx, -dy
+
+            x += dx
+            y += dy
+
+        return orderList
