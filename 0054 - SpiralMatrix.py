5800f746c7000d06bd9b15c2963c26942025a357
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-02 17:25:45 +0300

    New method visualization added to problem 54

--- 0054 - SpiralMatrix.py
+++ 0054 - SpiralMatrix.py
@@ -24,3 +24,31 @@ class Solution(object):
     @staticmethod
     def spiralOrder(self, matrix):
         return matrix and list(matrix.pop(0)) + self.spiralOrder(zip( * matrix)[ : : -1])
+    
+'''
+|1 2 3|      |6 9|      |8 7|      |4|  =>  |5|  =>  ||
+|4 5 6|  =>  |5 8|  =>  |5 4|  =>  |5|
+|7 8 9|      |4 7|
+
+spiral_order([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+= [1, 2, 3] + spiral_order([[6, 9],
+                            [5, 8],
+                            [4, 7]])
+
+= [1, 2, 3] + [6, 9] + spiral_order([[8, 7],
+                                     [5, 4]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + spiral_order([[4],
+                                              [5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + spiral_order([[5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + spiral_order([])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + []
+
+= [1, 2, 3, 6, 9, 8, 7, 4, 5]
+'''
