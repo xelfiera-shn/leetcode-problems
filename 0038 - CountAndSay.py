28dcb61f35e842a6df03b08f9ce4c8a7cca86ae8
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-10 23:17:24 +0300

    Problem 38 solved

--- 0038 - CountAndSay.py
+++ 0038 - CountAndSay.py
@@ -1,7 +1,24 @@
 class Solution(object):
-    def countAndSay(self, n):
-        """
-        :type n: int
-        :rtype: str
-        """
-        
+    @staticmethod
+    def countAndSay(n):
+        runLengthEncodes = ['1']
+
+        for i in range(1, n):
+            previousMember = runLengthEncodes[i - 1]
+
+            index = 0
+            currentMember = ''
+            while index < len(previousMember):
+                previousChar = list(previousMember)[index]
+                previousCharCount = 0
+
+                while index < len(previousMember) and list(previousMember)[index] == previousChar:
+                    index += 1
+                    previousCharCount += 1
+
+                currentMember += str(previousCharCount)
+                currentMember += previousChar
+
+            runLengthEncodes.append(currentMember)
+
+        return runLengthEncodes[-1]
