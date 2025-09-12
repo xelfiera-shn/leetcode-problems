ad53aa70ae7dcbaa19ce80c882203a13ea3a1d62
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-12 15:58:59 +0300

    Problem 53 solved but with memory limit exceeded solution

--- 0053 - MaximumSubarray.py
+++ 0053 - MaximumSubarray.py
@@ -1,7 +1,28 @@
+# Memory limit exceeded solution
 class Solution(object):
     @staticmethod
     def maxSubArray(nums):
-        pass
+        maxValue = nums[0]
+
+        sumDpMatrix = [[0 for _ in range(len(nums))] for _ in range(len(nums))]
+        for i in range(len(nums)):
+            if i > 0:
+                sumDpMatrix[0][i] += sumDpMatrix[0][i - 1] + nums[i]
+
+            else:
+                sumDpMatrix[0][i] += nums[i]
+
+            if sumDpMatrix[0][i] > maxValue:
+                maxValue = sumDpMatrix[0][i]
+
+        for yIndex in range(len(nums) - 1):
+            for xIndex in range(yIndex + 1, len(nums)):
+                sumDpMatrix[yIndex + 1][xIndex] = sumDpMatrix[yIndex][xIndex] - nums[yIndex]
+
+                if sumDpMatrix[yIndex + 1][xIndex] > maxValue:
+                    maxValue = sumDpMatrix[yIndex + 1][xIndex]
+
+        return maxValue
 
 # Time limit exceeded solution
 class Solution(object):
