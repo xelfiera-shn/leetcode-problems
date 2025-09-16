cd3bb31fc167dc28774d5595e2a67845f914c409
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-16 22:59:03 +0300

    Reformatted problem 31

--- 0031 - NextPermutation.py
+++ 0031 - NextPermutation.py
@@ -1,13 +1,11 @@
 class Solution(object):
     @staticmethod
     def nextPermutation(nums):
+        checker = False
         leftIndex = len(nums) - 2
         rightIndex = len(nums) - 1
-
-        checker = False
         while leftIndex >= 0:
             if nums[rightIndex] > nums[leftIndex]:
-
                 nums[leftIndex], nums[rightIndex] = nums[rightIndex], nums[leftIndex]
                 nums[leftIndex + 1 : ] = sorted(nums[leftIndex + 1 : ])
 
@@ -26,7 +24,6 @@ class Solution(object):
     @staticmethod
     def nextPermutation(nums):
         pointer = rightIndex = len(nums) - 1
-
         while pointer > 0 and nums[pointer - 1] >= nums[pointer]:
             pointer -= 1
 
@@ -40,4 +37,5 @@ class Solution(object):
             pointer -= 1; rightIndex = pointer
             while nums[rightIndex] <= nums[pointer]:
                 rightIndex += 1
+
             nums[pointer], nums[rightIndex] = nums[rightIndex], nums[pointer]
