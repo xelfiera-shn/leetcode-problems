dfb8657b8fa391739978152b5d6341198eb74dae
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-19 22:27:07 +0300

    Maybe worst solution of problem 39

--- 0039 - CombinationSum.py
+++ 0039 - CombinationSum.py
@@ -1,4 +1,24 @@
 class Solution(object):
     @staticmethod
     def combinationSum(candidates, target):
-        pass
+        results = []
+        candidates.sort()
+
+        def calculateCombination(combination, sum):
+            for candidate in candidates:
+                newSum = sum + candidate
+                newCombination = combination[ : : ]
+                newCombination.append(candidate)
+                newCombination.sort()
+
+                if newSum == target:
+                    if not newCombination in results:
+                        results.append(newCombination)
+
+                    break
+
+                elif newSum < target:
+                    calculateCombination(newCombination[ : : ], newSum)
+
+        calculateCombination([], 0)
+        return results
