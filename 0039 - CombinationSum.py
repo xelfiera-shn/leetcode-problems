5ec8b8a34272a4e894340d411209c53d5ebcec1d
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-19 22:40:48 +0300

    First solution optimized and added such as another solution of problem 39

--- 0039 - CombinationSum.py
+++ 0039 - CombinationSum.py
@@ -22,3 +22,26 @@ class Solution(object):
 
         calculateCombination([], 0)
         return results
+    
+# Another solution (previous solution optimized)
+class Solution(object):
+    @staticmethod
+    def combinationSum(candidates, target):
+        results = []
+        # candidates.sort()
+
+        def calculateCombination(startIndex, combination, sum):
+            for index in range(startIndex, len(candidates)):
+                newSum = sum + candidates[index]
+                newCombination = combination[ : : ]
+                newCombination.append(candidates[index])
+
+                if newSum == target:
+                    results.append(newCombination)
+                    # break
+
+                elif newSum < target:
+                    calculateCombination(index, newCombination[ : : ], newSum)
+
+        calculateCombination(0, [], 0)
+        return results
