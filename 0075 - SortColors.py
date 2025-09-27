4907d9ec5c4946f1168475e712763af7669f3e76
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-27 20:22:17 +0300

    Added Dutch national flag solution to problem 75

--- 0075 - SortColors.py
+++ 0075 - SortColors.py
@@ -18,4 +18,16 @@ class Solution(object):
 class Solution(object):
     @staticmethod
     def sortColors(nums):
-        pass
+        low, mid, high = 0, 0, len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+                
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
