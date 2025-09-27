a0a3dc624067df24da96fcb78d0722edda09ad0f
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-27 20:11:58 +0300

    A tricky solution added to problem 75

--- 0075 - SortColors.py
+++ 0075 - SortColors.py
@@ -1,7 +1,12 @@
 class Solution(object):
     @staticmethod
     def sortColors(nums):
-        pass
+        colorDict = {0: [], 1: [], 2: []}
+
+        for num in nums:
+            colorDict[num].append(num)
+
+        nums[ : ] = colorDict[0] + colorDict[1] + colorDict[2]
 
 # Another solution
 class Solution(object):
