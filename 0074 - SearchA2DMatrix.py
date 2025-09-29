7b4d7fba1f25e4d76ae8fc6fd160959d5f16619d
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-29 17:08:32 +0300

    Added brute force solution to problem 74

--- 0074 - SearchA2DMatrix.py
+++ 0074 - SearchA2DMatrix.py
@@ -1,41 +1,9 @@
 class Solution(object):
     @staticmethod
     def searchMatrix(matrix, target):
-        rowLength = len(matrix)
-        columnLength = len(matrix[0])
-        
-        upY, downY = 0, rowLength - 1
-        while upY <= downY:
-            midY = int((upY + downY) / 2)
-
-            if matrix[midY][columnLength - 1] == target:
-                return True
-            
-            elif matrix[midY][columnLength - 1] < target:
-                upY = midY + 1
-
-            else:
-                downY = midY - 1
-
-        if rowLength != 1 and upY == rowLength - 1:
-            return False
-        
-        if downY <= 0:
-            upY = -1
-        
-        left, right = 0, columnLength - 1
-        while left <= right:
-            mid = int((left + right) / 2)
-
-            if matrix[upY + 1][mid] == target:
-                return True
-            
-            elif matrix[upY + 1][mid] < target:
-                left = mid + 1
-
-            else:
-                right = mid - 1
+        for row in matrix:
+            for number in row:
+                if number == target:
+                    return True
 
         return False
-    
-Solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
