759e968a36bd416582bd541f373069f10c71226f
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-29 16:03:51 +0300

    Started solving problem 74

---
+++ 0074 - SearchA2DMatrix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    @staticmethod
+    def searchMatrix(matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
