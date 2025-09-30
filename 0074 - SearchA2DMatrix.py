356916dad0099b427b5fd1f9070cf2cc7402fc22
xelfiera <ahmet.sahin9663@gmail.com>
2025-09-30 13:45:33 +0300

    Added another solution to problem 74

--- 0074 - SearchA2DMatrix.py
+++ 0074 - SearchA2DMatrix.py
@@ -7,3 +7,23 @@ class Solution(object):
                     return True
 
         return False
+    
+# Another solution
+class Solution(object):
+    @staticmethod
+    def searchMatrix(matrix, target):
+        for i, row in enumerate(matrix):
+            if matrix[i][len(row) - 1] == target:
+                return True
+            
+            elif matrix[i][len(row) - 1] < target:
+                if i == len(matrix) - 1:
+                    return False
+                
+                continue
+
+            for number in row:
+                if number == target:
+                    return True
+                
+        return False
