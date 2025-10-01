c271b5f4ac739b3aff67f2b941999e4853e4502b
xelfiera <ahmet.sahin9663@gmail.com>
2025-10-01 23:03:20 +0300

    Problem 82 solved but with so brute force method

--- 0082 - RemoveDuplicatesFromSortedList-II.py
+++ 0082 - RemoveDuplicatesFromSortedList-II.py
@@ -6,23 +6,27 @@ class ListNode(object):
 class Solution(object):
     @staticmethod
     def deleteDuplicates(head):
-        if head == None or head.next == None:
-            return head
-        
         nonDuplicatedList = []
-
-        while head.next != None:
+        while head != None:
             nonDuplicatedList.append(head.val)
 
+            check = False
             while head.next != None and head.next.val == nonDuplicatedList[-1]:
                 head = head.next
+                check = True
+
+            if check:
+                nonDuplicatedList.pop()
 
             head = head.next
+
+        if len(nonDuplicatedList) == 0: return None
+        if len(nonDuplicatedList) == 1: return ListNode(nonDuplicatedList[0])
 
         currentNode = ListNode(nonDuplicatedList[1])
         head = ListNode(nonDuplicatedList[0], currentNode)
         for i in range(2, len(nonDuplicatedList)):
-            currentNode.next = ListNode(nonDuplicatedList[0])
+            currentNode.next = ListNode(nonDuplicatedList[i])
             currentNode = currentNode.next
 
         return head
