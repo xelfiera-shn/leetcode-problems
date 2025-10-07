2535945ec396c477bc5e4aae9a2a1c7c2dd3a5fc
xelfiera <ahmet.sahin9663@gmail.com>
2025-10-07 14:08:09 +0300

    Problem 36 solved with checking method

--- 0036 - ValidSudoku.py
+++ 0036 - ValidSudoku.py
@@ -1,4 +1,51 @@
 class Solution(object):
     @staticmethod
     def isValidSudoku(board):
-        pass
+        checkRowAndColumnTable = {
+            '0': [[], []],
+            '1': [[], []],
+            '2': [[], []],
+            '3': [[], []],
+            '4': [[], []],
+            '5': [[], []],
+            '6': [[], []],
+            '7': [[], []],
+            '8': [[], []],
+            '9': [[], []]
+        }
+
+        check3x3GridTable = {
+            (0, 0): [],
+            (0, 1): [],
+            (0, 2): [],
+            (1, 0): [],
+            (1, 1): [],
+            (1, 2): [],
+            (2, 0): [],
+            (2, 1): [],
+            (2, 2): []
+        }
+
+        for yIndex in range(len(board)):
+            for xIndex in range(len(board[yIndex])):
+                char = board[yIndex][xIndex]
+                if char == '.': continue
+
+                # Check column
+                if xIndex in checkRowAndColumnTable[char][0]: return False
+
+                else: checkRowAndColumnTable[char][0].append(xIndex)
+
+                # Check row
+                if yIndex in checkRowAndColumnTable[char][1]: return False
+
+                else: checkRowAndColumnTable[char][1].append(yIndex)
+
+                # Check grid
+                divisionX = xIndex // 3
+                divisionY = yIndex // 3
+                if char in check3x3GridTable[(divisionY, divisionX)]: return False
+
+                else: check3x3GridTable[(divisionY, divisionX)].append(char)
+
+        return True
