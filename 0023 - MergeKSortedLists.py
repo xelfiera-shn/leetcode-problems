38d5fdcd9b4e0babdcfadf7e5d60d96e0fbdfe1f
xelfiera <ahmet.sahin9663@gmail.com>
2025-10-17 17:12:31 +0300

    Started solving problem 23

---
+++ 0023 - MergeKSortedLists.py
@@ -0,0 +1,12 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        
