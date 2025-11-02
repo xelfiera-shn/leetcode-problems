2153e31797aa1d3f739ba45b1ecb5b2156da1395
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-02 21:23:11 +0300

    Implemented worst solution in the world for problem 118

--- 0118 - Pascal'sTriangle.py
+++ 0118 - Pascal'sTriangle.py
@@ -1,4 +1,30 @@
 class Solution(object):
     @staticmethod
     def generate(numRows):
-        pass
+        pascalTriangle = [[1]]
+        if numRows == 1:
+            return pascalTriangle
+
+        def CalculateAndInsertNextRow():
+            currentRow = pascalTriangle[-1]
+            if len(currentRow) == 1:
+                pascalTriangle.append([1, 1])
+                return
+            
+            firstIndex = 0
+            secondIndex = 1
+            nextRow = []
+            while secondIndex < len(currentRow):
+                nextRow.append(currentRow[firstIndex] + currentRow[secondIndex])
+
+                firstIndex += 1
+                secondIndex += 1
+
+            nextRow.insert(0, 1)
+            nextRow.append(1)
+            pascalTriangle.append(nextRow)
+
+        for i in range(numRows - 1):
+            CalculateAndInsertNextRow()
+
+        return pascalTriangle
