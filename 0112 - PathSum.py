ed23d7f8189e70a70016001828c4a7931ed35b0d
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-06 22:33:52 +0300

    Another solution added to problem 112

--- 0112 - PathSum.py
+++ 0112 - PathSum.py
@@ -22,3 +22,14 @@ class Solution(object):
             return False
         
         return searchTree(root, 0)
+    
+# Another solution
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        if not root: return False
+
+        if not root.left and not root.right and root.val == targetSum: return True
+
+        targetSum -= root.val
+
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
