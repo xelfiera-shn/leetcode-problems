2bdfd1572857ffcdd3c2b0811de23e5e5703f842
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-06 22:25:15 +0300

    Solution added to problem 112

--- 0112 - PathSum.py
+++ 0112 - PathSum.py
@@ -7,4 +7,18 @@ class TreeNode(object):
 class Solution(object):
     @staticmethod
     def hasPathSum(root, targetSum):
-        pass
+        if not root: return False
+
+        def searchTree(root, currentSum):
+            currentSum += root.val
+            if currentSum == targetSum and not root.left and not root.right: return True
+
+            if (root.left):
+                if (searchTree(root.left, currentSum)): return True
+
+            if (root.right):
+                if (searchTree(root.right, currentSum)): return True
+
+            return False
+        
+        return searchTree(root, 0)
