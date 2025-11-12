b01747c8c03d67ea7c1f1e362af8752206e129de
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-12 20:22:55 +0300

    Solved problem 119 with manuel combination method

--- 0119 - Pascal's Triangle-II.py
+++ 0119 - Pascal's Triangle-II.py
@@ -1,4 +1,12 @@
+from math import factorial as fc
 class Solution(object):
     @staticmethod
     def getRow(rowIndex):
-        pass
+        if rowIndex == 0: return [1]
+        
+        row = [1]
+        for index in range(1, rowIndex):
+            row.append(fc(rowIndex) / (fc(rowIndex - index) * fc(index)))
+
+        row.append(1)
+        return row
