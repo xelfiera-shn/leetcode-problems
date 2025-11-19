d41a8b9e0619052b7b914ff9236218193390fb23
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-19 20:38:48 +0300

    Time limit exceeded solution added to problem 120

--- 0120 - Triangle.py
+++ 0120 - Triangle.py
@@ -1,7 +1,16 @@
+# Time limit exceeded
 class Solution(object):
-    def minimumTotal(self, triangle):
-        """
-        :type triangle: List[List[int]]
-        :rtype: int
-        """
-        
+    @staticmethod
+    def minimumTotal(triangle):
+        sums = []
+
+        def goForward(row, column, sum):
+            if row == len(triangle) - 1:
+                sums.append(sum)
+                return
+            
+            for i in range(2):
+                goForward(row + 1, column + i, sum + triangle[row + 1][column + i])
+
+        goForward(0, 0, triangle[0][0])
+        return min(sums)
