277ca6a29bb73d236a75084eef822ea4ebae1238
xelfiera <ahmet.sahin9663@gmail.com>
2025-11-21 22:40:46 +0300

    Solved problem 120

--- 0120 - Triangle.py
+++ 0120 - Triangle.py
@@ -19,4 +19,18 @@ class Solution(object):
 class Solution(object):
     @staticmethod
     def minimumTotal(triangle):
-        pass
+        for y in range(1, len(triangle)):
+            for x in range(len(triangle[y])):
+                if x > 0 and x < len(triangle[y]) - 1:
+                    sum1 = triangle[y - 1][x - 1] + triangle[y][x]
+                    sum2 = triangle[y - 1][x] + triangle[y][x]
+
+                    triangle[y][x] = sum1 if sum1 < sum2 else sum2
+
+                elif x == 0:
+                    triangle[y][x] = triangle[y - 1][x] + triangle[y][x]
+
+                elif x == len(triangle[y]) - 1:
+                    triangle[y][x] = triangle[y - 1][x - 1] + triangle[y][x]
+
+        return min(triangle[-1])
